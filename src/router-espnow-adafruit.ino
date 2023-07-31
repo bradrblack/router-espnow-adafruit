db07ac434240c0c84fbe8f6545e36421b568140c
bradrblack <brad.black@gmail.com>
2023-07-31 11:37:01 -0400

    formatting src

--- src/router-espnow-adafruit.ino
+++ src/router-espnow-adafruit.ino
@@ -1,9 +1,8 @@
 //
 //  ESPNOW to Adafruite IO "Router"  - Brad Black (C) 2023
-//  
+//
 //  Get ESPNOW data from one or more sensor modules and relay/route to Adafruit IO Feeds
 //
-
 
 #include "esp_now.h"
 #include "esp_wifi.h"
@@ -20,13 +19,14 @@ const char* WIFI_PASS = "MY_PASSWORD";
 
 */
 
-//#define OPERATION_MODE // No Serial Debug
-#define TEST_MODE        // Serial Debug on for TESTING ONLY
+// #define OPERATION_MODE // No Serial Debug
+#define TEST_MODE // Serial Debug on for TESTING ONLY
 
-typedef struct msg {
-    char name[32];
-    float temperature;
-    float voltage;
+typedef struct msg
+{
+  char name[32];
+  float temperature;
+  float voltage;
 } msg;
 
 // Create a struct_message called sensorData
@@ -38,8 +38,8 @@ AdafruitIO_Feed *poolvolts = io.feed("voltage");
 AdafruitIO_Feed *pooltemp2 = io.feed("pooltemp2");
 AdafruitIO_Feed *poolvolts2 = io.feed("voltage2");
 
-
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
   memcpy(&sensorData, incomingData, sizeof(sensorData));
   Serial.print("Bytes received: ");
   Serial.println(len);
@@ -50,28 +50,30 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.print("Voltage: ");
   Serial.println(sensorData.voltage);
   Serial.println();
-  if(String(sensorData.name) == "Pool")
-    {
-        Serial.println("Sending to IO");
-  pooltemp->save(sensorData.temperature);
-  poolvolts->save(sensorData.voltage);
-    };
-  if(String(sensorData.name) == "Dev") {
-  Serial.println("Sending to IO");
-  pooltemp2->save(sensorData.temperature);
-  poolvolts2->save(sensorData.voltage);
+  if (String(sensorData.name) == "Pool")
+  {
+    Serial.println("Sending to IO");
+    pooltemp->save(sensorData.temperature);
+    poolvolts->save(sensorData.voltage);
+  };
+  if (String(sensorData.name) == "Dev")
+  {
+    Serial.println("Sending to IO");
+    pooltemp2->save(sensorData.temperature);
+    poolvolts2->save(sensorData.voltage);
   }
-  
 }
 
-void setup() {
+void setup()
+{
 
 #if defined(TEST_MODE)
   Serial.begin(115200);
-  while (!Serial) {}
+  while (!Serial)
+  {
+  }
   Serial.println("\nstarted");
 #endif
-
 
   // Bring up the WiFi connection
 
@@ -80,20 +82,20 @@ void setup() {
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   delay(500);
 
-  Serial.print( "\nConnected to " );
-  Serial.println( WIFI_SSID );
-  Serial.print( "Using IP address: " );
-  Serial.println( WiFi.localIP() );
+  Serial.print("\nConnected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("Using IP address: ");
+  Serial.println(WiFi.localIP());
 
-// Set the WiFi channel here to whatever your router is using - in this case it is "1" - this is required for WIFI_STA & ESPNOW at the same time
+  // Set the WiFi channel here to whatever your router is using - in this case it is "1" - this is required for WIFI_STA & ESPNOW at the same time
 
   esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK)
   {
-      Serial.println("Error initializing ESP-NOW");
-      return;
+    Serial.println("Error initializing ESP-NOW");
+    return;
   }
 
   // Once ESPNow is successfully Init, we will register for recv CB to
@@ -101,14 +103,15 @@ void setup() {
 
   Serial.println("ESP-NOW-INIT complete");
 
-    esp_now_register_recv_cb(OnDataRecv);
+  esp_now_register_recv_cb(OnDataRecv);
 
-   // connect to io.adafruit.com
+  // connect to io.adafruit.com
 
   io.connect();
   delay(1000);
   // wait for a connection
-  while(io.status() < AIO_CONNECTED) {
+  while (io.status() < AIO_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
@@ -117,8 +120,8 @@ void setup() {
   Serial.println(io.statusText());
 }
 
-void loop(void) {
+void loop(void)
+{
 
   io.run();
-
 }
